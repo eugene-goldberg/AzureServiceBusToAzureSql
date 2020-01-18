462875ba61cada097111f21c99a6f878a01f3ea0
aceteam.dfs@gmail.com <eugene@Eugenes-MacBook-Pro-2.local>
2020-01-18 11:13:53 -0600

    adding all local file

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+using System.Data.SqlClient;
+
+namespace ServiceBusReader
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://servicebusnamespace001968.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=8Eh/E41lAfG2RIbhLrVaNeqIwtONXgbldDFs1gUj62c=";
+        const string QueueName = "BasicQueue";
+        static IQueueClient queueClient;
+
+        static SqlConnection connection = null;
+
+        public static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "myazuresql001968.database.windows.net"; 
+                builder.UserID = "sqladmin001968";            
+                builder.Password = "@Myazuresql001968";     
+                builder.InitialCatalog = "mySampleDatabase";
+
+                connection = new SqlConnection(builder.ConnectionString);
+                connection.Open();
+
+            // Register the queue message handler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of concurrent calls to the callback ProcessMessagesAsync(), set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether the message pump should automatically complete the messages after returning from user callback.
+                // False below indicates the complete operation is handled by the user callback as in ProcessMessagesAsync().
+                AutoComplete = false
+            };
+
+            // Register the function that processes messages.
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message.
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+             
+             string query = "INSERT INTO [SalesLT].[messages] (message) VALUES(@Message)"; 
+             SqlCommand cmd = new SqlCommand(query, connection);
+             cmd.Parameters.AddWithValue("@Message", Encoding.UTF8.GetString(message.Body));
+                    
+             cmd.ExecuteNonQuery();
+             Console.WriteLine("Records Inserted Successfully");
+            
+            // Complete the message so that it is not received again.
+            // This can be done only if the queue Client is created in ReceiveMode.PeekLock mode (which is the default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been closed, you can choose to not call CompleteAsync() or AbandonAsync() etc.
+            // to avoid unnecessary exceptions.
+        }
+
+        // Use this handler to examine the exceptions received on the message pump.
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
